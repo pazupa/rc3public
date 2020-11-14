44d165f11d23d01c6c02388ebbbd19624fd62e8b
Patrizia Eva Hedwig Pschera <pschera@campus.tu-berlin.de>
2020-11-14 22:22:07 +0100

    ... as png

--- [A]WaterFall3_pipo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="[A]WaterFall3_pipo" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="Pipoya RPG Tileset 32x32/[A]_type3/not_animation/[A]WaterFall3_pipo.png" width="256" height="192"/>
-</tileset>
